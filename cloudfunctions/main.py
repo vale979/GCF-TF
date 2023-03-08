a9ef1bb908dc467d86830f34d4f5ba659ce10001
Valerian Pratama <valerian.pratama@dentsu.com>
2023-03-08 13:56:20 +0700

    Update main.py

--- cloudfunctions/main.py
+++ cloudfunctions/main.py
@@ -4,4 +4,4 @@ from datetime import date
 def get_date():
     today = date.today()
     date_str = today.strftime("%Y-%b-%d")
-    return f"Current date: {date_str}", 200
+    return f"Current date is {date_str}", 200
